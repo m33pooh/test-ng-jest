ed3a51bab9245771f522de8b727ac3edae20c979
rendyssaputra231 <rendyssaputra231@gmail.com>
2025-11-18 18:10:41 +0000

    fix build

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,10 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DashboardComponent } from './components/dashboard.component';
-import { DocumentListComponent } from './components/document-list.component';
-import { DocumentListItemComponent } from './components/document-list-item.component';
 import { UserSwitcherComponent } from './components/user-switcher.component';
-import { DocumentSubmissionComponent } from './components/document-submission.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DashboardComponent, DocumentListComponent, DocumentListItemComponent, UserSwitcherComponent, DocumentSubmissionComponent],
+  imports: [RouterOutlet, UserSwitcherComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
